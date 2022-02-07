45f0fd938841a7d1d3ef53285233a474f4728a11
Athanasia Monika Mowinckel <a.m.mowinckel@psykologi.uio.no>
2022-02-07 09:46:12 +0100

    convert onload from warning to message

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,4 @@
 .onLoad <- function(libname, pkgname) {
   if(!lcbc_font_installed())
-    warning("You might want to run 'lcbc_install_font()' to install the LCBC branded font on your system.")
+    message("You might want to run 'lcbc_install_font()' to install the LCBC branded font on your system.")
 }
