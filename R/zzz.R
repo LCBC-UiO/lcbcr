8bd850c880e5778b5a5ce5316650e1bc9857e026
Athanasia Monika Mowinckel <a.m.mowinckel@psykologi.uio.no>
2022-02-07 16:52:08 +0100

    switch to packageStartupMessage

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,4 @@
 .onLoad <- function(libname, pkgname) {
   if(!lcbc_font_installed())
-    message("You might want to run 'lcbc_install_font()' to install the LCBC branded font on your system.")
+    packageStartupMessage("You might want to run 'lcbc_install_font()' to install the LCBC branded font on your system.")
 }
